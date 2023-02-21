33f7861bd1440749c78721016f3125a9b4d96542
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-02-21 22:33:55 +0530

    modified article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Models):
+    title = models.charField(max_length=100)
+    slug = models.slugField()
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
