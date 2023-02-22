ff0208946189e24eebe93654e8a6eda84b14e33b
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-02-22 23:01:19 +0530

    create migration and display data to template using models

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Article(models.Models):
-    title = models.charField(max_length=100)
-    slug = models.slugField()
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
+
+
+
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Article
 
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/index.html', {'articles': articles})
     # return HttpResponse("Hello, world. You're at the articles index.")
