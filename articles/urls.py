59e34b707615d5b8ae80a1c5482cf6ee5b7eb0ee
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-02-27 23:59:59 +0530

    added named routes and icon

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='articles'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:slug>/', views.article_detail),
+    path('', views.index, name='list'),
+    path('<slug:slug>/', views.article_detail, name='article_detail'),
 ]
